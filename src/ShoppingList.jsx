3402c67073945e8827e8fae28171c487c25f5d5d
Al Amin <alaminkhan10009@gmail.com>
2024-12-18 21:58:13 +0600

    adding list component

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,7 +1,16 @@
-export default function ShoppingList(){
+import ShoppingListItem from "./ShoppingListItem"
+export default function ShoppingList({items}){
 
     return (
+        <div>
 
+
+       <ul>
+        {items.map((i)=>        <ShoppingListItem  key={i.id}  item={i.item} quantity={i.quantity} completed={i.completed} />               )}
+       </ul>
+
+
+        </div>
     )
 }
 
