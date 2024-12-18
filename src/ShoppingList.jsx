9f25789c57d6f49a562f8aa5b69c39598d05d0f3
Al Amin <alaminkhan10009@gmail.com>
2024-12-18 21:03:57 +0600

    second commit

---
+++ src/ShoppingList.jsx
@@ -0,0 +1,16 @@
+export default function ShoppingList(){
+
+    return (
+
+    )
+}
+
+
+
+// const data= [
+//     { item: 'eggs', quantity: 12, completed: false},
+//     { item: 'milk', quantity: 1, completed: true},
+//     { item: 'chicken breasts', quantity: 4, completed: false},
+//     { item: 'carrots', quantity: 6, completed: true}
+//   ]
+  
